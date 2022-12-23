fb78cbf814084236b99349a4c70ab5135d19db07
vitalydem <121297390+vitalydem@users.noreply.github.com>
2022-12-23 16:56:49 +0200

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -8,6 +8,6 @@ import { Product } from '../products';
   styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent {
-  @Input() product!: Product;
+  @Input() product!: Product | undefined;
   @Output() notify = new EventEmitter();
 }
