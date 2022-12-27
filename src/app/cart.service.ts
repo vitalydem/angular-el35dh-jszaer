03018bb341a21c79a2e99d4240e9867262902ed1
vitalydem <121297390+vitalydem@users.noreply.github.com>
2022-12-27 14:02:11 +0200

    4 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 
@@ -6,7 +7,7 @@ import { Injectable } from '@angular/core';
 })
 export class CartService {
   items: Product[] = [];
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -19,5 +20,11 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.httpClient.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 }
